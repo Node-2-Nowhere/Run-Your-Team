12e9ba9dda566c7882d3bffcd9329edce2c0e4d1
chrisjames1724 <christopher.cummings28@gmail.com>
2024-04-15 11:10:49 -0600

    seeds testing data

--- seeds/result-seeds.js
+++ seeds/result-seeds.js
@@ -1,6 +1,17 @@
 const { Results } = require("../models");
 
-const resultsData = [];
+const resultsData = [
+    {   
+        home_win: true, 
+        away_win: false, 
+        game_played: true,
+    },
+    {
+        home_win:  false, 
+        away_win: true, 
+        game_played: true, 
+    },
+];
 
 const seedResults = () => Results.bulkCreate(resultsData);
 
--- seeds/team-seeds.js
+++ seeds/team-seeds.js
@@ -4,39 +4,39 @@ const teamData = [
   {
     team_name: "Wombats",
     league_id: 1,
-    win_count: 3,
-    games_played: 4,
+    win_count: 1,
+    games_played: 2,
   },
   {
     team_name: "Squirrels",
     league_id: 1,
-    win_count: 2,
-    games_played: 4,
-  },
-  {
-    team_name: "Pigeons",
-    league_id: 1,
     win_count: 1,
-    games_played: 4,
+    games_played: 2,
   },
-  {
-    team_name: "Seagulls",
-    league_id: 1,
-    win_count: 2,
-    games_played: 4,
-  },
-  {
-    team_name: "Orcas",
-    league_id: 1,
-    win_count: 3,
-    games_played: 4,
-  },
-  {
-    team_name: "Elephants",
-    league_id: 1,
-    win_count: 1,
-    games_played: 4,
-  },
+  // {
+  //   team_name: "Pigeons",
+  //   league_id: 1,
+  //   win_count: 1,
+  //   games_played: 4,
+  // },
+  // {
+  //   team_name: "Seagulls",
+  //   league_id: 1,
+  //   win_count: 2,
+  //   games_played: 4,
+  // },
+  // {
+  //   team_name: "Orcas",
+  //   league_id: 1,
+  //   win_count: 3,
+  //   games_played: 4,
+  // },
+  // {
+  //   team_name: "Elephants",
+  //   league_id: 1,
+  //   win_count: 1,
+  //   games_played: 4,
+  // },
 ];
 
 const seedTeam = () => Team.bulkCreate(teamData);
