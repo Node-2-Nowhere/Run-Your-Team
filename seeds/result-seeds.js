d07a0a3d0bbb3030381c72e24ae1d1c8f6fe0295
chrisjames1724 <christopher.cummings28@gmail.com>
2024-04-15 11:10:49 -0600

    seeds testing data

--- seeds/result-seeds.js
+++ seeds/result-seeds.js
@@ -1,39 +1,18 @@
 const { Result } = require("../models");
 
 const resultsData = [
-  {
-    win: 1,
-    team_id: 1,
-    match_id: 1,
-  },
-  {
-    win: 1,
-    team_id: 2,
-    match_id: 2,
-  },
-  {
-    win: 1,
-    team_id: 2,
-    match_id: 3,
-  },
-  {
-    win: 1,
-    team_id: 2,
-    match_id: 4,
-  },
-  {
-    win: 1,
-    team_id: 2,
-    match_id: 5,
-  },
-  {
-    win: 1,
-    team_id: 1,
-    match_id: 6,
-  },
+    {   
+        home_win: true, 
+        away_win: false, 
+        game_played: true,
+    },
+    {
+        home_win:  false, 
+        away_win: true, 
+        game_played: true, 
+    },
 ];
-console.log("------------------")
-console.log(Result);
-const seedResults = () => Result.bulkCreate(resultsData);
+
+const seedResults = () => Results.bulkCreate(resultsData);
 
 module.exports = seedResults;
