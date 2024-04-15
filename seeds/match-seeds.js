35785b0f9dc2bade4d4da5a164b63fccb518f92e
chrisjames1724 <christopher.cummings28@gmail.com>
2024-04-15 11:10:49 -0600

    seeds testing data

--- seeds/match-seeds.js
+++ seeds/match-seeds.js
@@ -3,18 +3,13 @@ const { Match } = require("../models");
 const matchData = [
   {
     match_date: "2024-03-28",
-    home_team_id: 2,
-    away_team_id: 3,
-  },
-  {
-    match_date: "2024-04-02",
     home_team_id: 1,
     away_team_id: 2,
   },
   {
-    match_date: "2024-04-05",
-    home_team_id: 3,
-    away_team_id: 4,
+    match_date: "2024-04-02",
+    home_team_id: 2,
+    away_team_id: 1,
   },
 ];
 
