3bbff178ec622b2f6416d28cc30857eecdf805be
ItsToastCrumb <160540794+ItsToastCrumb@users.noreply.github.com>
2024-04-15 10:32:27 -0600

    updated routes folder to controllers to meet MVC criteria, added quotes to strings in seed files, fixed date formatting in seed files, updated server.js to route to controllers folder

--- seeds/team-seeds.js
+++ seeds/team-seeds.js
@@ -2,37 +2,37 @@ const { Team } = require("../models");
 
 const teamData = [
   {
-    team_name: Wombats,
+    team_name: "Wombats",
     league_id: 1,
     win_count: 3,
     games_played: 4,
   },
   {
-    team_name: Squirrels,
+    team_name: "Squirrels",
     league_id: 1,
     win_count: 2,
     games_played: 4,
   },
   {
-    team_name: Pigeons,
+    team_name: "Pigeons",
     league_id: 1,
     win_count: 1,
     games_played: 4,
   },
   {
-    team_name: Seagulls,
+    team_name: "Seagulls",
     league_id: 1,
     win_count: 2,
     games_played: 4,
   },
   {
-    team_name: Orcas,
+    team_name: "Orcas",
     league_id: 1,
     win_count: 3,
     games_played: 4,
   },
   {
-    team_name: Elephants,
+    team_name: "Elephants",
     league_id: 1,
     win_count: 1,
     games_played: 4,
