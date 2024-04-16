00b0faccdd7e733fca54ac7dabdb3d4ee1b10316
ItsToastCrumb <160540794+ItsToastCrumb@users.noreply.github.com>
2024-04-16 13:03:42 -0600

    updated login.js with DOM references, updated sessions cookies with parameters

--- server.js
+++ server.js
@@ -12,8 +12,13 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // const sess = {
-//   secret: 'Super secret secret',
-//   cookie: {},
+//   secret: 'Team Tracker login',
+//   cookie: {
+//     maxAge: 24*60*60*1000,
+//     httpOnly: true,
+//     secure: false,
+//     sameSite: 'strict',
+//   },
 //   resave: false,
 //   saveUninitialized: true,
 //   store: new SequelizeStore({
